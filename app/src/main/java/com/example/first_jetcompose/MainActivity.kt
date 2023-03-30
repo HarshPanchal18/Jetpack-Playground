3fe936733476d26c91a91bad530faf031cd498c3
Harsh Panchal <harshhhh1803@gmail.com>
2023-03-30 12:19:17 +0530

    Added Floating Action Button

--- app/src/main/java/com/example/first_jetcompose/MainActivity.kt
+++ app/src/main/java/com/example/first_jetcompose/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.first_jetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.first_jetcompose.ui.theme.FirstjetcomposeTheme
 
@@ -20,6 +24,13 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android\nComing through the JetPack Compose ")
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    IconFloatingActionButton()
                 }
             }
         }
@@ -36,5 +47,17 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     FirstjetcomposeTheme {
         Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+private fun IconFloatingActionButton() {
+    FloatingActionButton(onClick = {},
+    shape = RectangleShape) {
+        Icon(
+            imageVector = Icons.Outlined.Star,
+            contentDescription = null
+        )
     }
 }
