c3af149e7738baded9d4b155f8b3bd154979bd71
Harsh Panchal <harshhhh1803@gmail.com>
2023-04-02 10:47:40 +0530

    Added expanded view which previews custom object file

--- app/src/main/java/com/example/first_jetcompose/MainActivity.kt
+++ app/src/main/java/com/example/first_jetcompose/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.first_jetcompose
 
 import android.content.Context
-import android.content.res.Configuration
+import android.graphics.Color.parseColor
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -21,23 +25,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.first_jetcompose.ui.theme.FirstjetcomposeTheme
+import com.example.first_jetcompose.ui.theme.SampleData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FirstjetcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Column(
-                        modifier = Modifier
-                            .verticalScroll(rememberScrollState())
-                            .fillMaxSize()
-                            .padding(5.dp),
-                    ) {
-                        for (i in 1..10) {
-                            MessageCard(Message("Android", "$i. Jetpack Compose\n"))
-                        }
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {  // A surface container using the 'background' color from the theme
+                    Column(modifier = Modifier.fillMaxSize().padding(5.dp)) {
+                        Conversation(SampleData.conversationSample)
                     } // Column
                 } // Surface
             } // FirstjetcomposeTheme
@@ -51,118 +49,61 @@ data class Message(val author:String,val body:String)
 fun MessageCard(msg:Message) {
     val mContext = LocalContext.current // Fetching the local context for using the Toast
     Row(
-        modifier = Modifier
-            .padding(5.dp)
-            .fillMaxWidth()
+        modifier = Modifier.padding(5.dp).fillMaxWidth()
             .shadow(elevation = 2.dp, shape = RoundedCornerShape(10.dp))
-            .background(Color.Yellow)
-            .clickable { mToast(mContext) },
+            .background("#a0ca74".color).clickable { mToast(mContext) },
         horizontalArrangement = Arrangement.Center,
-        ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
             contentDescription = null,
-            modifier = Modifier.padding(5.dp)
-                .background(Color.LightGray, shape = CircleShape)
-                .border(2.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
+            modifier = Modifier.padding(5.dp).background(Color.LightGray, shape = CircleShape)
+                .border(2.dp, MaterialTheme.colors.secondary, CircleShape))
         Spacer(modifier = Modifier.width(8.dp)) // Add a horizontal space between the image and the column
-        Column(
-            modifier = Modifier.align(Alignment.CenterVertically)
-        ) {
-            Text(
-                text = msg.author,
+        var isExpanded by remember { mutableStateOf(false) } // We keep track if the message is expanded or not in this variable
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+        Column(modifier = Modifier.align(Alignment.CenterVertically)
+            .clickable { isExpanded = !isExpanded }) {
+            Text(text = msg.author,
                 color = MaterialTheme.colors.primaryVariant,
                 style = MaterialTheme.typography.subtitle1,
-                modifier = Modifier.fillMaxWidth()
-            )
+                modifier = Modifier.fillMaxWidth())
             Spacer(modifier = Modifier.height(4.dp)) // Add a vertical space between the author and message texts
-            Surface(shape = MaterialTheme.shapes.small, elevation = 3.dp) {
-                Text(
-                    text = msg.body,
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(text = msg.body,
                     style = MaterialTheme.typography.body1,
-                    //modifier = Modifier.padding(4.dp).fillMaxWidth()
-                )
+                    modifier = Modifier.padding(4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1)
             }
         }
     }
-}
+} // MessageCard
 
 // Function to generate a Toast
 private fun mToast(context: Context) {
     Toast.makeText(context, "Toasted", Toast.LENGTH_LONG).show()
 }
 
-@Preview(name = "Light Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    showBackground = true)
-@Preview(
-    name = "Dark Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true)
 @Composable
-fun PreviewCard() {
+fun Conversation(message:List<Message>) {
+    LazyColumn {
+        items(message) { MessageCard(msg = it) }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
     FirstjetcomposeTheme {
-        MessageCard(Message("Collage", "SDJ International College, Vesu"))
+        Conversation(SampleData.conversationSample)
     }
 }
 
-/*
-@Composable
-fun Greeting(name: String) {
-    Box(
-        modifier = Modifier.size(300.dp)
-    ) {
-        Column {
-            Text(
-                text = "Hello $name!",
-                color = Color.Cyan,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "Just started the Jetpack",
-                color = Color.Green
-            )
-        }
-    }
-}
-
-@Composable
-fun Images() {
-    Column {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = null,
-            modifier = Modifier.background(Color.LightGray),
-        )
-        Icon( // for the vectors
-            imageVector = Icons.Default.AccountBox,
-            contentDescription = null,
-            modifier = Modifier
-                .background(Color.Blue)
-                .align(Alignment.CenterHorizontally)
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FirstjetcomposeTheme {
-        Greeting("Android")
-        Images()
-    }
-}
-
-//@Preview
-@Composable
-private fun IconFloatingActionButton() {
-    FloatingActionButton(onClick = {},
-    shape = RectangleShape) {
-        Icon(
-            imageVector = Icons.Outlined.Star,
-            contentDescription = null
-        )
-    }
-}
-*/
+// Adding extension for apply the HexCoded color
+// .background("HEXCODE".color)
+val String.color get()=Color(parseColor(this))
