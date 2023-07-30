e53ceda49a908634d7d919979ad32b66368da1de
Harsh Panchal <harshhhh1803@gmail.com>
2023-07-30 14:24:23 +0530

    Implemented Bufferring for consumers to consume data in less time

--- app/src/main/java/com/example/first_jetcompose/FlowsActivity.kt
+++ app/src/main/java/com/example/first_jetcompose/FlowsActivity.kt
@@ -2,7 +2,6 @@ package com.example.first_jetcompose
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,25 +10,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.example.first_jetcompose.ui.theme.FirstjetcomposeTheme
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class FlowsActivity : ComponentActivity() {
-
-    private val channel = Channel<Int>()
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,10 +38,15 @@ class FlowsActivity : ComponentActivity() {
                                 flowProducer().toList() // consume the emitted flow elements into the list
                             Log.d("Values in list", data.toString())
 
-                            flowProducer()
-                                .map { it * 2 }
-                                .filter { it < 8 } // filter data by the condition
-                                .collect { Log.d("Collecting", "$it") } // a terminal operator who starts the flow
+                            val time = measureTimeMillis {
+                                flowProducer()
+                                    .buffer(3) // Adding buffer process to consume data swiftly with less time
+                                    .collect {
+                                        delay(1500)
+                                        Log.d("Collecting", "$it")
+                                    } // a terminal operator who starts the flow
+                            }
+                            Log.d("Time taken",time.toString())
                         }
                     }
                 }
