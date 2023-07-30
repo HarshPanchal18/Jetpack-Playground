7e2545b6176cb4040c8f11e21e9043157ed97be1
Harsh Panchal <harshhhh1803@gmail.com>
2023-07-30 14:55:41 +0530

    Implemented multiple Context switching through .flowOn() method

--- app/src/main/java/com/example/first_jetcompose/FlowsActivity.kt
+++ app/src/main/java/com/example/first_jetcompose/FlowsActivity.kt
@@ -11,14 +11,15 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.example.first_jetcompose.ui.theme.FirstjetcomposeTheme
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.withContext
 
 class FlowsActivity : ComponentActivity() {
 
@@ -33,20 +34,24 @@ class FlowsActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     LaunchedEffect(Unit) {
-                        GlobalScope.launch {
-                            val data =
-                                flowProducer().toList() // consume the emitted flow elements into the list
-                            Log.d("Values in list", data.toString())
-
-                            val time = measureTimeMillis {
-                                flowProducer()
-                                    .buffer(3) // Adding buffer process to consume data swiftly with less time
-                                    .collect {
-                                        delay(1500)
-                                        Log.d("Collecting", "$it")
-                                    } // a terminal operator who starts the flow
-                            }
-                            Log.d("Time taken",time.toString())
+                        GlobalScope.launch(Dispatchers.Main) {
+                            // Context switching
+                            flowProducer()
+                                .map {
+                                    delay(1000)
+                                    it * 2
+                                    Log.d("Map thread", "Map thread" + Thread.currentThread().name)
+                                }
+                                .flowOn(Dispatchers.IO) // will run on IO thread, works on upstream (Decides the context of the above given block)
+                                .filter {
+                                    delay(500)
+                                    Log.d("Filter thread", "Filter thread" + Thread.currentThread().name)
+                                    it < 8
+                                }
+                                .flowOn(Dispatchers.Main) // will run on Main thread
+                                .collect { //will run on main thread
+                                    Log.d("Collecting", Thread.currentThread().name)
+                                } // a terminal operator who starts the flow
                         }
                     }
                 }
@@ -55,10 +60,20 @@ class FlowsActivity : ComponentActivity() {
     }
 
     private fun flowProducer() = flow<Int> {
+        //withContext(Dispatchers.IO) {
+        /*
+        because of:
+        java.lang.IllegalStateException: Flow invariant is violated:
+            Flow was collected in [StandaloneCoroutine{Active}@993c81, Dispatchers.Main],
+            but emission happened in [DispatchedCoroutine{Active}@e744126, Dispatchers.IO].
+            Please refer to 'flow' documentation or use 'flowOn' instead
+        */
         val list = listOf(1, 2, 3, 4, 5)
         repeat(list.size) {
             delay(1000)
+            Log.d("Emitting", Thread.currentThread().name)
             emit(it)
         }
+        //}
     }
 }
